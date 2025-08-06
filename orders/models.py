e06dffbcb0bd8b959f0eb5492d1d1b725a1bd0e7
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-06 21:49:59 +0530

    17948

--- orders/models.py
+++ orders/models.py
@@ -1,19 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-# UserProfile model to extend the built-in Django User
-class UserProfile(models.Model):
-    # One-to-One relationship with the built-in User model
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name= 'profile')
-
-    # Additional profile fields
-    name = models.CharField(max_length=100) # Full name of the user
-    email = models.EmailField(max_length=254) # Email address of the user
-    phone_number = models.CharField(max_length=15, blank=True, null=True) # Optional phone number
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        # This will display in the admin and when printing the object
-        return f"{self.user.username}'s Profile" 
-
-
-
+        return self.name
