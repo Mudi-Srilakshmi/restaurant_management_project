50e35f4a107aebbacb75fc41b7bc7a70fb3d9090
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-07 14:14:25 +0530

    17948

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from .models import MenuItem
+from django.http import JsonResponse
 
-
-# Create your views here.
+def menu_api(request):
+    items = MenuItem.objects.all()
+    data = [
+        {
+            'id': item.id,
+            'name': item.name,
+            'description': item.description,
+            'price': float(item.price)
+        }
+        for item in items
+    ]
+    return JsonResponse(data, safe=False)
