5543decad50204fe8ad63d9bd966239ff1e05d6e
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-21 13:27:54 +0530

    36329

--- home/views.py
+++ home/views.py
@@ -1,11 +1,16 @@
-from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
-def home(request):
-    context = {
-        "restaurant_name": "My Restaurant",
-        "phone_number": settings.RESTAURANT_PHONE,
-    }
-    return render(request, "home/home.html", context)
+# View to handle feedback form
+def feedback_view(request):
+    if request.method == 'POST': # If form is submitted
+        form = FeedbackForm(request.POST)
+        if form.is_valid(): # Validate form
+            form.save() # Save feedback to database
+            return redirect('feedback') # Redirect to same page after submit
+
+    else:
+        form = FeedbackForm() # Empty form for GET request
+
+    return render(request, 'home/feedback.html', {'form': form})
     
-        
