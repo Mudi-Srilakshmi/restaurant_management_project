45cad7f2a749239a15b959248dc2896db992dea6
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-25 16:31:01 +0530

    38659

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,14 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    # your app urls...
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('orders.urls')), # Include app urls
+]
+
+# For serving uploaded media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
