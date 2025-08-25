7eedf6eefb278960b7c2bd7ace1f190a26b29ef4
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-25 21:31:40 +0530

    38658

--- orders/models.py
+++ orders/models.py
@@ -1,19 +1,12 @@
 from django.db import models
 
-class MenuItem(models.Model):
-    """
-    MenuItem model:
-    - name: name of the food item
-    - description: short details about the item
-    - price: price of the item
-    - image: uploaded image (stored in MEDIA folder)
-    """
-
-    name = models.CharField(max_length=100) # Item name
-    description = models.TextField() # Item description
-    price = models.DecimalField(max_digits=6, decimal_places=2) # Price
-    image = models.ImageField(upload_to='menu_images/') # Image upload path: MEDIA/menu_images/
+# This model stores the restaurant's address
+class RestaurantAddress(models.Model):
+    name = models.CharField(max_length=100) # Name of the restaurant
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10)
 
     def __str__(self):
-        return self.name
-        
+        return f"{self.name}, {self.city}"
