49e8c539f34d9f37c9e7cfeaaae5dd2490b3bfb9
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-25 16:04:28 +0530

    38659

--- orders/models.py
+++ orders/models.py
@@ -3,18 +3,17 @@ from django.db import models
 class MenuItem(models.Model):
     """
     MenuItem model:
-    - Stores details of each menu item (name, description, price, image).
-    - ImageField is used for uploading and storing images in MEDIA folder.
+    - name: name of the food item
+    - description: short details about the item
+    - price: price of the item
+    - image: uploaded image (stored in MEDIA folder)
     """
 
-    name = models.CharField(max_length=100) # Name of the menu item
-    description = models.TextField() # Short description
+    name = models.CharField(max_length=100) # Item name
+    description = models.TextField() # Item description
     price = models.DecimalField(max_digits=6, decimal_places=2) # Price
-    image = models.ImageField(upload_to='menu_images/') # Upload folder: MEDIA/menu_images/ 
+    image = models.ImageField(upload_to='menu_images/') # Image upload path: MEDIA/menu_images/
 
     def __str__(self):
         return self.name
-
-
-
-
+        
