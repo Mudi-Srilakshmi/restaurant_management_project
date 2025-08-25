8b5c225ce1200a721f7bc73d8b3c758999c7f2db
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-25 21:52:31 +0530

    38658

--- home/settings.py
+++ home/settings.py
@@ -1,7 +1,6 @@
-import os
-
-# Media files configuration (for storing uploaded images)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+INSTALLED_APPS = [
+    ...
+    'orders',
+]
 
 
