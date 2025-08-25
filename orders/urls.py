34b0c9f81dd9c47abd43eb5f818763d2348ef41d
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-25 16:10:13 +0530

    38659

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('menu/', views.menu_view, name='menu'), # Menu page
+    path('menu/', views.menu_view, name='menu'), # URL for menu page
 ]
+   
