45a78a08ad8e023f33b6dfc094cf0fe8775c46bf
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-26 11:21:07 +0530

    38657

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-# This model stores the restaurant's address
+# Option 2: Store restaurant info (name) in the database
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# existing model for addresses
 class RestaurantAddress(models.Model):
-    name = models.CharField(max_length=100) # Name of the restaurant
+    name = models.CharField(max_length=100) # Name of the restaurant branch
     street = models.CharField(max_length=200)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=10)
 
     def __str__(self):
-        return f"{self.name}, {self.city}"
+        return f"{self.name}, {self.city}" 
