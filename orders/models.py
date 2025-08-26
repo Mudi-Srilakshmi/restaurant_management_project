46c70d03ec4e2ecd9de415f1799c5a467f1c2294
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-26 14:34:18 +0530

    38655

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+# Model to represent a menu item
 class MenuItem(models.Model):
     name = models.CharField(max_length=100) # Name of the menu item
     description = models.TextField(blank=True, null=True) # Optional description
     price = models.DecimalField(max_digits=6, decimal_places=2) # Price of the item
-    is_available = models.BooleanField(default=True) # Availability status
+    is_available = models.BooleanField(default=True) # Show only available items
 
     def __str__(self):
         return self.name
