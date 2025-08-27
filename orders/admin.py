09d537304ebfc331164b7f58da3cd57e10bc1ec1
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-27 21:30:36 +0530

    39608

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import RestaurantInfo, RestaurantAddress
 
+# Register models in admin so we can add/edit restautant info and addresses
 admin.site.register(RestaurantInfo)
 admin.site.register(RestaurantAddress)
 
