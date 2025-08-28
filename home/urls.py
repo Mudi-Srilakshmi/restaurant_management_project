9565671c48134dbc07953efeffc28d00b16d4ba9
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-28 17:36:13 +0530

    39605

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('orders.urls')), # Your app routes
+    path('', include('orders.urls')), # homepage points to orders app
 ]
-
-# To serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
