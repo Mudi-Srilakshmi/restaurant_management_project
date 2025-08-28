3be24932d2caaabfa1d757d952f84f5de5dfd2d7
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-28 18:43:47 +0530

    40126

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class MenuItem(models.Model):
+class Restaurant(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    description = models.TextField(blank=True, null=True)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15) # Added phone number field
 
     def __str__(self):
         return self.name
