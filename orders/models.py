94c23ce0c741da51b66e84f5a94cc9024d3c062c
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-28 17:04:19 +0530

    39605

--- orders/models.py
+++ orders/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField() # Menu item description
-    price = models.DecimalField(max_digits=6, decimal_places=2) # Price
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-    # New field for image, stored in MEDIA_ROOT/menu_images/
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
