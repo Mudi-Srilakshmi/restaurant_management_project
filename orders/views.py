78c0daf6133103fcfaede1583dfecbfe5c0f1ccf
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-28 17:19:35 +0530

    39605

--- orders/views.py
+++ orders/views.py
@@ -1,34 +1,10 @@
-from django.shortcuts import render, redirect
-from django.core.mail import send_mail
-from django.conf import settings
-from .forms import ContactForm
+from django.shortcuts import render
+from .models import MenuItem
 
-def contact_view(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            # Get form data
-            name = form.cleaned_data["name"]
-            email = form.cleaned_data["email"]
-            message = form.cleaned_data["message"]
-
-            # Email subject and content
-            subject = f"New Contact Form Submission from {name}"
-            body = f"Name: {name}\nEmail: {email}\n\nMessage:\n{message}"
-
-            # Send email
-            send_mail(
-                subject,
-                body,
-                settings.DEFAULT_FROM_EMAIL, # Sender
-                [settings.RESTAURANT_EMAIL], # receiver
-            )
-
-            # Redirect after success
-            return redirect("contact_success")
-
+def homepage(request):
+    query = request.GET.get('q') # 'q' will come from the search input 
+    if query:
+        menu_items = MenuItem.objects.filter(name__icontains=query) # case-insensitive search
     else:
-        form = ContactForm()
-
-    return render(request, "orders/contact.html", {"form": form})
-    
+        menu_items = MenuItem.objects.all() # show all if no search
+    return render(request, 'homepage.html', {'menu_items': menu_items, 'query': query})
