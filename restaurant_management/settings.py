11ce134e70c4b705467cf624d6af09dbff61cab9
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-29 13:31:56 +0530

    40125

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -1,16 +1,4 @@
-# Email backend (for development use console backend)
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+import os
 
-# If you want real email sending, configure SMTP like this:
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "yourgmail.com"
-# EMAIL_HOST_PASSWORD = "yourpassword"
-
-DEFAULT_FROM_EMAIL = "yourgmail@gmail.com" # sender email
-RESTAURANT_EMAIL = "restaurant@example.com" # restaurant receiving email
-
-
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
