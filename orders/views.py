f2a601c5481b5a68bd06458eeea4f66867adbe32
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-29 13:06:21 +0530

    40125

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,7 @@ from .models import Restaurant
 
 def homepage(request):
     # Get the first restaurant from the database
-    restaurant = Restaurant.objects.first()
+    restaurant = Restaurant.objects.first() # Fetch th restaurant
 
     return render(request, "homepage.html", {"restaurant": restaurant})
     
