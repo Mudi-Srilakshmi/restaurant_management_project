0a0d2c96450e4c5416de42714a97865d9e49047e
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-08-29 21:43:55 +0530

    40124

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'), # Homepage route
+
+    # Other URLS...
+
+    # New URL for restaurant info page
+    path('restaurant-info/', views.restaurant_info, name='restaurant_info'),
 ]
+    
