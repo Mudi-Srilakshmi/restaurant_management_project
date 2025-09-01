b3236a4e05f655a46d5be5b47b8e63fe05d79828
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-01 22:23:50 +0530

    40123

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('contact/', include('contact.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')), # your app name here
 ]
