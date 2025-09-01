9e7480f4d8fac4386406876dac21d0934a285cdc
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-01 17:40:01 +0530

    40122

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,5 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('orders.urls')), # include app urls
+    path('contact/', include('contact.urls')),
 ]
-
-# serve uploaded files
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
