dc041e564baa06f8c56dc1938c33e38973222ffe
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-01 21:34:23 +0530

    40123

--- orders/models.py
+++ orders/models.py
@@ -1,15 +1,8 @@
 from django.db import models
 
-class Restaurant(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100)
-    address = models.TextField()
-    phone_number = models.CharField(max_length=15)
-    logo = models.ImageField(upload_to='restaurant_logos/', blank=True, null=True)
-    
-    # New fields for information page
-    history =models.TextField(blank=True, null=True)
-    mission = models.TextField(blank=True, null=True)
-    
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
     def __str__(self):
         return self.name
-
