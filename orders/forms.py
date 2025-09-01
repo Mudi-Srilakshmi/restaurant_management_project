3cbef9a5e9d724c7ac28f10d5cfa1100b4c0b3b6
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-01 17:16:36 +0530

    40122

--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 
+# ContactForm class using Django's Form class
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label="Your Name")
-    email = forms.EmailField(label="Your Email")
-    message = forms.CharField(widget=forms.Textarea, label="Your Message")
+    name = forms.CharField(
+        max_length=100,
+        required=True,
+        label="Your Name"
+    )
+    email = forms.EmailField( # EmailField does email validation automatically
+        required=True,
+        label="Your Email"
+    )
+    message = forms.CharField(
+        widget=forms.Textarea, # textarea instead of single-line input
+        required=True,
+        label="Your Message"
+    )
+    
