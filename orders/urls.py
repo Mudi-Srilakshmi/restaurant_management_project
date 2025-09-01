0f38a4ae51e257f129e900b8d100e1dd4a2624fa
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-01 21:56:38 +0530

    40123

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import contact_view
+from .views import home_view, add_to_cart
 
 urlpatterns = [
-    path('', contact_view, name='contact'),
+    path('', home_view, name='home'),
+    path('add-to-cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
 ]
