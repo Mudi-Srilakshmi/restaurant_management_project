3fc96d6fa70cf126841419efaf6c106564ad94c9
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-04 22:14:26 +0530

    42323

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path('faq/', views.faq, name='faq'), # FAQ page
 ]
