238224749dfcc6781398d117145cf5b7c9eee4ac
Mudi-Srilakshmi <srilakshmimudi21@gmail.com>
2025-09-05 17:05:29 +0530

    42873

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from datetime import datetime
 
-def homepage(request):
-    # Get the current date and time
-    current_time = datetime.now()
-
-    # Pass it to the template
-    return render(request, 'home/homepage.html', {'current_time': current_time})
+def privacy_policy(request):
+    """
+    Renders the hardcoded privacy policy page
+    """
+    return render(request, "orders/privacy.html")
     
